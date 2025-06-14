6b647bb306ec3e979a6eed12c3ab7705543764f6
Ignacio <ignacio.cabrera@goatlab.io>
2025-06-14 11:46:17 +0200

    add tasks-core

---
+++ packages/tasks-core/src/ShouldQueue.types.ts
@@ -0,0 +1,46 @@
+export type TaskStatusName =
+  | 'QUEUED'
+  | 'RUNNING'
+  | 'FAILED'
+  | 'COMPLETED'
+  | 'CANCELLED'
+
+export type JsonValue =
+  | string
+  | number
+  | boolean
+  | null
+  | { [x: string]: JsonValue }
+  | JsonValue[]
+
+export type JsonObject = { [x: string]: JsonValue }
+
+export type InputType = {
+  [x: string]: JsonValue
+} & {
+  [x: string]: JsonValue
+}
+
+export type OutputType = void | JsonObject
+export type UnknownInputType = {}
+
+export interface TaskStatus<T extends InputType = UnknownInputType> {
+  id: string
+  name: string
+  status: TaskStatusName
+  output: string
+  attempts: number
+  created: string
+  nextRun: string | null
+  nextRunMinutes: number | null
+  payload: T
+}
+
+export interface TaskConnector<TInput> {
+  schedule(params: {
+    taskName: string
+    postUrl: string
+    taskBody: TInput
+  }): Promise<Omit<TaskStatus, 'payload'>>
+  getStatus(id: string): Promise<TaskStatus>
+}
