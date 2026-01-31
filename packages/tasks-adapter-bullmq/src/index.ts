f117b256734a1d5d52489f4cf3e359705e8c2530
Ignacio Cabrera <ignacio.cabrera@goatlab.io>
2026-01-31 09:37:54 -0300

    feat(09-02): export BullMQDispatchListener from package index
    - Add BullMQDispatchListener and BullMQDispatchListenerConfig to index.ts
    - Follows existing export pattern with .js extension in import paths
    - BullMQDispatchListener now available via @goatlab/tasks-adapter-bullmq import

--- packages/tasks-adapter-bullmq/src/index.ts
+++ packages/tasks-adapter-bullmq/src/index.ts
@@ -4,6 +4,7 @@ export type {
 } from "./BullMQConnector.js";
 export { BullMQConnector } from "./BullMQConnector.js";
 export { BullMQDispatchConnector, type BullMQDispatchConnectorConfig } from "./BullMQDispatchConnector.js";
+export { BullMQDispatchListener, type BullMQDispatchListenerConfig } from "./BullMQDispatchListener.js";
 export { RedisConnectionPool } from "./RedisConnectionPool.js";
 export { DispatchLuaScripts } from "./DispatchLuaScripts.js";
 export { MultiTenantWorkerManager, createMultiTenantWorkerManager, type MultiTenantWorkerManagerConfig } from "./MultiTenantWorkerManager.js";
